f2ba27ded7a016758cc6488c840d08483a4c4014
paulKrasnov <pramuk@live.ru>
2022-02-20 20:18:28 +1000

    refactored Add method

--- ConsoleApp1/Feature1.cs
+++ ConsoleApp1/Feature1.cs
@@ -1,11 +1,8 @@
 ﻿namespace ConsoleApp1;
 public class Class1
 {
-	public int Add()
+	public int Add(int x1, int x2)
 	{
-		int x1 = 1;
-		int x2 = 2;
-		int sum = x1 + x2;
-		return sum;
+		return x1 + x2;
 	}
 }
